4dddd857689ccc28671644397459d4835b4f9a06
Moldirkab <moldirkabdykarim@gmail.com>
2024-12-22 18:07:13 +0600

    getting two numbers and clculating the sum

---
+++ ExceptionsLearn.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.Info;
+
+import java.util.Scanner;
+
+public class ExceptionsLearn {
+        public static void main(String[] args) {
+            System.out.println("enter two numbers:");
+            Scanner scanner=new Scanner(System.in);
+            int n1=scanner.nextInt();
+            int n2=scanner.nextInt();
+            System.out.println("the sum is :" + (n1+n2));
+
+        }
+}
+
+
+
+
+
+
+
+
+//
+//
+//}
